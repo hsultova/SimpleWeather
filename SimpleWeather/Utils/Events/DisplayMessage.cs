405bcb1854f4f08d6444f78a6d33a61baf80e156
hsultova <hsultova@users.noreply.github.com>
2021-01-20 21:56:34 +0200

    Add event for displaying a message

---
+++ SimpleWeather/Utils/Events/DisplayMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleWeather.Utils.Events
+{
+	public enum DisplayMessageType
+	{
+		Non,
+		Error,
+		Info
+	}
+	public class DisplayMessageEventArgs
+	{
+		public string Message { get; set; }
+		public DisplayMessageType Тype { get; set; }
+	}
+
+}
--- SimpleWeather/ViewModels/Base/BaseViewModel.cs
+++ SimpleWeather/ViewModels/Base/BaseViewModel.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using SimpleWeather.Utils.Events;
+using SimpleWeather.Utils.General;
 
 namespace SimpleWeather.ViewModels.Base
 {
 	public class BaseViewModel : INotifyPropertyChanged
 	{
+		#region INotifyPropertyChanged
 		public event PropertyChangedEventHandler PropertyChanged;
 
 		protected void OnPropertyChanged([CallerMemberName] string propertyName = "") 
 			=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		#endregion
+
+		public event EventHandler<DisplayMessageEventArgs> DisplayMessageHandler;
+		protected void OnDisplayMessage(DisplayMessageEventArgs args) => DisplayMessageHandler?.Invoke(this, args);
 	}
 }
