b564ef23fd8a125d781120b90dca4aa546ac02b5
hsultova <hsultova@users.noreply.github.com>
2021-01-20 22:15:53 +0200

    Add message to be displayed in the main view

--- SimpleWeather/MainWindowViewModel.cs
+++ SimpleWeather/MainWindowViewModel.cs
@@ -1,14 +1,37 @@
-﻿using SimpleWeather.ViewModels;
+﻿using System;
+using SimpleWeather.Utils.Events;
+using SimpleWeather.ViewModels;
+using SimpleWeather.ViewModels.Base;
 
 namespace SimpleWeather
 {
-	public class MainWindowViewModel
+	public class MainWindowViewModel :BaseViewModel
 	{
 		public MainWindowViewModel()
 		{
 			CurrentViewModel = new ForecastViewModel();
+			CurrentViewModel.DisplayMessageHandler += OnDisplayError;
 		}
 
-		public object CurrentViewModel { get; set; }
+		private void OnDisplayError(object sender, DisplayMessageEventArgs e)
+		{
+			Error = e.Message;
+		}
+
+		public ForecastViewModel CurrentViewModel { get; set; }
+
+		private string _error;
+		public string Error
+		{
+			get
+			{
+				return _error;
+			}
+			set
+			{
+				_error = value;
+				OnPropertyChanged(nameof(Error));
+			}
+		}
 	}
 }
