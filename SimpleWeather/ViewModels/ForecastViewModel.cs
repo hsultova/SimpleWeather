f99f8b6073f947b13f49d138b32f91d06aa1b7bf
hsultova <hsultova@users.noreply.github.com>
2021-01-20 22:18:41 +0200

    Update forecast view model
    Add Headline property. Extract methods for filling data - cities and forecast. Use temp json files instead of api calls, they are limited.

--- SimpleWeather/ViewModels/ForecastViewModel.cs
+++ SimpleWeather/ViewModels/ForecastViewModel.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using Newtonsoft.Json;
 using SimpleWeather.Managers;
+using SimpleWeather.Managers.Abstract;
 using SimpleWeather.Models;
 using SimpleWeather.Utils.General;
 using SimpleWeather.ViewModels.Base;
@@ -12,8 +15,10 @@ namespace SimpleWeather.ViewModels
 	{
 		public ForecastViewModel()
 		{
-
+			_weatherManager = new AccuWeatherApiManager();
 		}
+
+		private IWeatherAPI _weatherManager;
 
 		private string _query;
 		public string Query
@@ -57,17 +62,31 @@ namespace SimpleWeather.ViewModels
 			}
 		}
 
-		private string _icon;
-		public string Icon
+		private Headline _headline;
+		public Headline Headline
 		{
 			get
 			{
-				return _icon;
+				return _headline;
 			}
 			set
 			{
-				_icon = value;
-				OnPropertyChanged(nameof(Icon));
+				_headline = value;
+				OnPropertyChanged(nameof(Headline));
+			}
+		}
+
+		private string _iconPath;
+		public string IconPath
+		{
+			get
+			{
+				return _iconPath;
+			}
+			set
+			{
+				_iconPath = value;
+				OnPropertyChanged(nameof(IconPath));
 			}
 		}
 
@@ -78,11 +97,53 @@ namespace SimpleWeather.ViewModels
 
 		private async void OnSearch()
 		{
-			var cities = await AccuWeatherApiManager.GetCities(Query);
+			await FillCityData();
+			await FillForecastData();
+
+			IconPath = "/SimpleWeather;component/Images/" + DailyForecast.Day.Icon + "-s.png";
+		}
+
+		private async Task FillForecastData()
+		{
+			//var response = await _weatherManager.GetDailyForecastOneDay(SelectedCity?.Key);
+			//if (response.StatusCode != System.Net.HttpStatusCode.OK)
+			//{
+			//	OnDisplayMessage(new DisplayMessageEventArgs { Message = response.ReasonPhrase, Тype = DisplayMessageType.Error });
+			//}
+
+			//var json = await response.Content.ReadAsStringAsync();
+			//var forecast = JsonConvert.DeserializeObject<DailyForecastOneDay>(json);
+
+			DailyForecastOneDay forecast;
+			using (System.IO.StreamReader r = new System.IO.StreamReader("forecast_json.json"))
+			{
+				var json = r.ReadToEnd();
+				forecast = JsonConvert.DeserializeObject<DailyForecastOneDay>(json);
+			}
+
+			Headline = forecast.Headline;
+			DailyForecast = forecast.DailyForecasts.FirstOrDefault();
+		}
+
+		private async Task FillCityData()
+		{
+			//var response = await _weatherManager.GetCities(Query);
+			//if (response.StatusCode != System.Net.HttpStatusCode.OK)
+			//{
+			//	OnDisplayMessage(new DisplayMessageEventArgs { Message = response.ReasonPhrase, Тype = DisplayMessageType.Error });
+			//}
+
+			//string json = await response.Content.ReadAsStringAsync();
+			//var cities = JsonConvert.DeserializeObject<List<City>>(json);
+
+			List<City> cities;
+			using (System.IO.StreamReader r = new System.IO.StreamReader("cities_json.json"))
+			{
+				var json = r.ReadToEnd();
+				cities = JsonConvert.DeserializeObject<List<City>>(json);
+			}
+
 			SelectedCity = cities.FirstOrDefault();
-			var dailyForecastOneDay = await AccuWeatherApiManager.GetDailyForecastOneDay(SelectedCity.Key);
-			//Use FirstOrDefault daily forecast to test. Change this.
-			Icon = "/SimpleWeather;component/Images/" + dailyForecastOneDay.DailyForecasts.FirstOrDefault().Day.Icon + "-s.png";
 		}
 	}
 }
