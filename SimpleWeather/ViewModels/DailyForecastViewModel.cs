bf86bfdc6e5596bd591f5133f2f1dc6e7e7e3fe4
hsultova <hsultova@users.noreply.github.com>
2021-01-21 22:37:46 +0200

    Add DailyForecastViewModel

---
+++ SimpleWeather/ViewModels/DailyForecastViewModel.cs
@@ -0,0 +1,33 @@
+﻿using SimpleWeather.Models;
+
+namespace SimpleWeather.ViewModels
+{
+	public class DailyForecastViewModel
+	{
+		public DailyForecastViewModel(DailyForecast model, CurrentConditions conditions)
+		{
+			DailyForecast = model;
+			_currentConditions = conditions;
+		}
+
+		private CurrentConditions _currentConditions;
+		public DailyForecast DailyForecast { get; set; }
+
+		public string IconPath
+		{
+			get
+			{
+				if(_currentConditions.IsDayTime)
+				{
+					return "/SimpleWeather;component/Images/" + DailyForecast.Day.Icon + "-s.png"; 
+				}
+				else
+				{
+					return "/SimpleWeather;component/Images/" + DailyForecast.Night.Icon + "-s.png";
+				}
+			}
+		}
+
+		public string Description => _currentConditions.IsDayTime ? DailyForecast.Day.IconPhrase : DailyForecast.Night.IconPhrase;
+	}
+}
