2449df2863f137f5aff7052336bebcace79cad52
hsultova <hsultova@users.noreply.github.com>
2021-03-19 16:59:19 +0200

    Make the app single instance

--- SimpleWeather/App.xaml.cs
+++ SimpleWeather/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,17 @@ namespace SimpleWeather
 	/// </summary>
 	public partial class App : Application
 	{
+		Mutex mutex;
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			string mutexName = "SimpleWeather";
+			mutex = new Mutex(false, mutexName, out bool createdNew);
+			if(!createdNew)
+			{
+				Shutdown();
+			}
+		}
 	}
 }
